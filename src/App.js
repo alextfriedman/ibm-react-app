442ec31c806c2aa9c92a212c03ee4bac5be44c5e
alexandrafriedman <alextfriedman@gmail.com>
2022-07-25 11:14:30 -0400

    installed axios, retrieved data from API in console

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
 import React from "react";
+import axios from "axios";
+import { useEffect } from "react";
 
 const App = () => {
+
+    useEffect(() => {
+        axios({
+            url: "https://jsonplaceholder.typicode.com/posts",
+            method: "GET",
+            dataResponse: "json"
+        }).then((res) => {
+            console.log(res);
+        })
+    }, [])
 
     return <div>This was made from scratch!</div>
 
