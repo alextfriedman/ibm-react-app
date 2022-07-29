19525ccb4f06966b8bfa727f4d0ca02d2d167aea
alexandrafriedman <alextfriedman@gmail.com>
2022-07-29 17:30:55 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from "react";
 import axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const App = () => {
+    const [allData, setAllData] = useState([]);
 
     useEffect(() => {
         axios({
@@ -10,11 +11,30 @@ const App = () => {
             method: "GET",
             dataResponse: "json"
         }).then((res) => {
-            console.log(res);
+            console.log(res.data);
+
+            return (
+                    function displayData () {
+                        return (
+                            <section>
+                                <h1>Here is the data</h1>
+                                <div>
+                                    {res.data.map((info) => {
+                                        return (
+                                            
+                                        )
+                                    })}
+                                </div>
+                            </section>
+                        );
+                    }
+            );
         })
     }, [])
 
-    return <div>This was made from scratch!</div>
+    return (
+    <div>This was made from scratch!</div>
+    );
 
 };
 
